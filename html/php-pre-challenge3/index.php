a2f71239a31d5f7533264dace75afdee8862f810
Yuzunoha <studysteadystandby@gmail.com>
2020-05-07 23:11:09 +0900

    ひな形を書いた。先は長そうだ

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -1,6 +1,33 @@
 <?php
+
+declare(strict_types=1);
+
 $limit = $_GET['target'];
 
 $dsn = 'mysql:dbname=test;host=mysql';
 $dbuser = 'test';
 $dbpassword = 'test';
+
+PhpPreChallenge3::run($limit);
+
+class PhpPreChallenge3
+{
+  public static function run(string $limitStr)
+  {
+    try {
+      $limit = self::getLimitInt($limitStr);
+    } catch (Exception $e) {
+      echo json_encode($e->getMessage());
+      exit();
+    }
+
+    echo $limit;
+  }
+  public static function getLimitInt(string $limitStr): int
+  {
+    if (true) {
+      throw new Exception('invalid limit : ' . $limitStr);
+    }
+    return 0;
+  }
+}
