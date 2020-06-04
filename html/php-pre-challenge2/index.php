cf61b6c80b645866b1bcaeae5ead7a6791ddeed0
Robert Anthony Kirkman <therobertkirkman@gmail.com>
2020-06-04 16:18:32 +0900

    修正しました

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -4,52 +4,25 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 $length = count($array);
-$比較 = 0;
-
-echo "<h1>Bubble Sort Algorithm</h2>";
-
-for ( $i = 0; $i < $length; $i++ ) {
-    for( $j = 0; $j < $length - 1; $j++ ){
-        $比較++;
-        if( $array[ $j ] > $array[ $j + 1 ] ) {
-            $tmp = $array[ $j + 1 ];
-            $array[ $j + 1 ] = $array[ $j ];
-            $array[ $j ] = $tmp;
-        }
-    }
-}
-// 修正はここまで
-
-echo "<pre>";
-echo '<h4>' . $比較. ' 比較回数 / Comparisons </h4>';
-print_r($array);
-echo "</pre>";
-
-
-
-echo "<h1>Bidirectional Bubble Sort Algorithm</h2>";
-
-
-$array = explode(',', $_GET['array']);
-
-// 修正はここから
-$comparisons = 0;
 $start = -1;
 
+// don't need to declare $amount as variable as this if statement sets it automatically
+// $amount= count($array); FOLLOWING IF STATEMENT IS AUTOMATIICALLY DECLARED WHEN USING IF STATEMENT
 if( !$amount = $length ){
     return $array;
 }
 while( $start < $amount ){
     ++$start;
     --$amount;
-    
+    // start begins the sorting from the first array element
+    // amount begins the sorting from the last array element
+
     for( $i = 0 ; $i < $amount; ++$i){
         if( $array[ $i ] > $array[ $i + 1 ]){
             $tmp = $array[ $i ];
             $array[ $i ] = $array[ $i + 1 ];
             $array[ $i + 1 ] = $tmp;
         }
-        $comparisons++;
     }
     for( $i = $amount; --$i >= $start;){
         if( $array[ $i ] > $array[ $i + 1 ]){
@@ -57,14 +30,10 @@ while( $start < $amount ){
             $array[ $i ] = $array[ $i + 1 ];
             $array[ $i + 1 ] = $tmp;
         }
-        $comparisons++;
     }
 }
 
 // 修正はここまで
-
 echo "<pre>";
-echo '<strong>並べ替え後 / After sorting</strong><br>';
-echo '<h4>' . $comparisons . ' 比較回数 / Comparisons </h4>';
 print_r($array);
 echo "</pre>";
