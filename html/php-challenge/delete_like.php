f0bd1979699a37931ba5d07d3e1188f9406142a1
Ryu-y-10 <ryu@RyunosukenoMacBook-Pro.local>
2020-06-18 14:31:56 +0900

    add:いいね取り消し機能追加

---
+++ html/php-challenge/delete_like.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if (isset($_SESSION['id'])){
+
+$delete_likes = $db->prepare('DELETE FROM likes WHERE like_member_id=? AND like_post_id=?');
+$delete_likes->execute(array(
+    $_REQUEST['like_member_id'],
+    $_REQUEST['like_post_id']
+));
+}
+header('Location: index.php');
+exit();
