1245a9dfb704489135e75d808f80a0e88c6f1cbc
fu10ta <fu99ta@gmail.com>
2020-08-17 15:22:56 +0900

    first commit

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,7 +3,13 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-
+    for($j = 0; $j < count($array)-1; $j++){
+        if($array[$j] > $array[$j+1]){
+            $tmp = $array[$j];
+            $array[$j] = $array[$j+1];
+            $array[$j+1] = $tmp;
+        }
+    }
 }
 // 修正はここまで
 
